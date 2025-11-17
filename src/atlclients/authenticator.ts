c6c195c6b25db77289a9399104cdd90a91a34746
Jovana Dunisijevic <jdunisijevic@atlassian.com>
2025-11-17 17:53:02 +1100

    just testing that I have permissions to push PRs

--- src/atlclients/authenticator.ts
+++ src/atlclients/authenticator.ts
@@ -3,7 +3,7 @@ import { AccessibleResource, DetailedSiteInfo, OAuthProvider } from './authInfo'
 /**
  * Authenticator encapsulates the information needed to authenticate with an OAuth service.
  */
-
+// just a random comment, ignore me please
 export interface Authenticator {
     getOAuthSiteDetails(
         provider: OAuthProvider,
